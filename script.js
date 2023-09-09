ed8bf386ac54b01bafc10b35db1a1376a00b4537
Ujjwal Upadhyay <ujjwalupadhyay2003@gmail.com>
2023-09-09 12:07:05 +0530

    Added script.js file

---
+++ script.js
@@ -0,0 +1,16 @@
+const inputText = document.getElementById("input-text");
+const imgArea = document.getElementById("img-area");
+const btn = document.getElementById("btn")
+
+
+function scanner(scannerText) {
+    imgArea.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${scannerText}`;
+}
+
+
+btn.addEventListener('click', () => {
+
+    scanner(inputText.value);
+    imgArea.style.display = "block";
+})
+console.log("exited");
